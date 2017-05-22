89dc4b7f53ecf2630f348d3a03ba24799530dd17
SOMPO\lcamlibel <lcamlibel@sompo-us.com>
2017-05-22 16:37:10 -0400

    login redirection code is added

--- src/sompo.app/appconfig/portal.routes.js
+++ src/sompo.app/appconfig/portal.routes.js
@@ -6,33 +6,37 @@
 
   /** @ngInject */
   function routesConfig($stateProvider) {
-    // $stateProvider
-    //     .state('dashboard', {
-    //         url: '/dashboard',
-    //         templateUrl: "sompo.app/views/dashboard/dashboard.template.html",
-    //         controller: 'dashboardController',
-    //         controllerAs: 'vm',
-    //         resolve: {
-    //             /** @ngInject */
-    //             isAuthenticated: function($rootScope,authService) {
-    //                 authService.isAuthenticated(['admin', 'external_agent_broker'], null);
-    //                   $rootScope.$broadcast('menuChanged', 'menuname');
-    //                 return true;
-    //             }
-    //         }
-    //     });
+    $stateProvider
+      .state('dashboard', {
+        url: '/dashboard',
+        templateUrl: "sompo.app/views/dashboard/dashboard.template.html",
+        controller: 'dashboardController',
+        controllerAs: 'vm',
+        resolve: {
+          /** @ngInject */
+          isAuthenticated: function ($rootScope, authService) {
+            var response = authService.isAuthenticated('admin', 'external_agent_broker', null);
+            if (response === true) {
+              $rootScope.$broadcast('menuChanged', 'dashboard');
+            } else {
+              window.location = '/login';
+            }
+            return response;
+          }
+        }
+      });
   }
   /** @ngInject */
   function setup(sideMenuService, appScopeService) {
     //add scope for login
     //TODO: change this
     appScopeService.add('policy_service');
-    // sideMenuService.add({
-    //     name: 'edelivery',
-    //     state: 'edelivery',
-    //     title: 'eDelivery',
-    //     icon: 'fa fa-clock-o fa-lg'
-    // });
+    sideMenuService.add({
+        name: 'edelivery',
+        state: 'edelivery',
+        title: 'eDelivery',
+        icon: 'fa fa-clock-o fa-lg'
+    });
   }
 
 })();
